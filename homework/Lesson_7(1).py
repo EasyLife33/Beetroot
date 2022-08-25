a12dde161a47d0c453877f7390d30ef49572498b
Yura Hradiuk <111080782+EasyLife33@users.noreply.github.com>
2022-08-25 12:24:27 +0300

    Delete Lesson_7(1).py

--- homework/Lesson_7(1).py
+++
@@ -1,7 +0,0 @@
-abc = input()
-magazine = dict()
-abc = abc.strip()
-for f in abc.split(' '):
-    magazine.update({f: abc.count(f)})
-for n, w in magazine.items():
-    print(f'All words "{n}" in line: {w}')
