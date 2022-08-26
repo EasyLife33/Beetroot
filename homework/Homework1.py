df24613c84a4e081fd01848a81ba63b0f99aab1e
Yura Hradiuk <111080782+EasyLife33@users.noreply.github.com>
2022-08-26 21:39:06 +0300

    Update and rename Homework1.py to Lesson_3.py

--- homework/Homework1.py
+++
@@ -1,6 +0,0 @@
-# Task 1
-
-my_name = 'Yura'
-week_day = 'Tuesday'
-print('Good day!',my_name[0:4]," ",week_day[0:7],'is a perfect day to learn some python.')
-
---
+++ homework/Lesson_3.py
@@ -0,0 +1,17 @@
+# Task 1
+my_name = 'Yura'
+week_day = 'Tuesday'
+print('Good day!',my_name[0:4]," ",week_day[0:7],'is a perfect day to learn some python.')
+
+
+#Task2
+First_name = 'Yura'
+Second_name = 'Hradiuk'
+print("First name:",First_name)
+print("Second name:",Second_name)
+
+
+#Task 3
+a = 10
+b = 5
+print("Addition:=",a+b,"Substraction:=",a-b,"Divison:=",a*b,"Multiplication:=",a/b,"Exponent=",a%b,"Modulus:=",a**b,"Floor Divison:=",a//b)
