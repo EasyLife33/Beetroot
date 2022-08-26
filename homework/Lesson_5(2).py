5eb32898dc5a24b150f4577001e7382e0be1c5fa
Yura Hradiuk <111080782+EasyLife33@users.noreply.github.com>
2022-08-26 21:45:08 +0300

    Delete Lesson_5(2).py

--- homework/Lesson_5(2).py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ") 
-current_age = int(input("Enter your age: ")) 
-next_year =  ( current_age + 1)
-print(f'Hello {name} on your next birthday you’ll be {next_year}.')
